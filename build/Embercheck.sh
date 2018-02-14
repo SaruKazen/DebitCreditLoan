a12ff2acd1f5e15286a98bdcacd534c7aecf05fb
saravanakumar.n <saravanakumar.n@zohocorp.com>
2018-02-14 17:39:34 +0530

    Installation requirement done

--- build/Embercheck.sh
+++
@@ -1,6 +0,0 @@
-curl --insecure -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash \
-&& . ~/.bashrc \
-&& cd ~/.nvm \
-&& nvm install 5.6.0 \
-&& nvm use 5.6.0 \
-&& npm install ember-cli@2.18.2
---
+++ build/downloadNVM.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+ ~/
+cd ~/.nvm ;
+nvm install v9.5.0 ;
+nvm use 9.5.0 ;
+npm install -g ember-cli@3.0.0;
